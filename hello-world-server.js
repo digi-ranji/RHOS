ae421bffba2395b0677cc5b49b8e28383946c3ee
digi-ranji <ranjith.tr@gmail.com>
2020-03-12 09:21:20 +0800

    Sample Node Added 2

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World to RHOS 4.3 \n');
+    res.end('<b> Hello World to RHOS 4.3.1</b> \n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
